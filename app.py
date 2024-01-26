5ab136f9d9cfec7f422fe0a6bef4570d03292435
llachkovic <905667@student.uek.krakow.pl>
2024-01-26 23:13:45 +0100

    added post route mapping to get probability response from the model

--- app.py
+++ app.py
@@ -1,11 +1,42 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+import joblib
+import numpy as np
+from sklearn.linear_model import LogisticRegression
+from sklearn.preprocessing import StandardScaler
+
+from data_types import DataPoint
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+@app.route('/', methods=['POST'])
+def predict():
+    if request.is_json:
+        data = request.get_json()
+
+        try:
+            request_data = DataPoint(**data)
+            model: LogisticRegression = joblib.load('models/linear_regression.joblib')
+            scaler: StandardScaler = joblib.load('models/linear_regression.scaler.joblib')
+            data_array = np.array([request_data.__dict__[key] for key in request_data.__dict__])
+            data_array_scaled = scaler.transform(data_array.reshape(1, -1))
+            print(data_array_scaled)
+            prediction = model.predict_proba(data_array_scaled)[0]
+
+            response = {
+                'probability_good': prediction[0],
+                'probability_bad': prediction[1]
+            }
+            return jsonify(response)
+
+        except TypeError as e:
+            error_result = {'error': f'Invalid request format - {e}'}
+            return jsonify(error_result), 400
+
+    else:
+        # Return a response if the request does not contain JSON data
+        error_result = {'error': 'Invalid request format'}
+        return jsonify(error_result), 400
 
 
 if __name__ == '__main__':
---
+++ data_types.py
@@ -0,0 +1,40 @@
+class DataPoint:
+    def __init__(self, Attribute5, Attribute8, Attribute13, Attribute1_A13,
+                 Attribute1_A14, Attribute3_A34, Attribute4_A41, Attribute4_A42,
+                 Attribute4_A43, Attribute6_A61, Attribute6_A62, Attribute6_A63,
+                 Attribute6_A64, Attribute6_A65, Attribute7_A71, Attribute7_A72,
+                 Attribute7_A73, Attribute7_A74, Attribute7_A75, Attribute9_A93,
+                 Attribute12_A121, Attribute12_A122, Attribute12_A123,
+                 Attribute12_A124, Attribute14_A143, Attribute15_A151,
+                 Attribute15_A152, Attribute15_A153, Attribute19_A191,
+                 Attribute19_A192):
+        self.Attribute5 = Attribute5
+        self.Attribute8 = Attribute8
+        self.Attribute13 = Attribute13
+        self.Attribute1_A13 = Attribute1_A13
+        self.Attribute1_A14 = Attribute1_A14
+        self.Attribute3_A34 = Attribute3_A34
+        self.Attribute4_A41 = Attribute4_A41
+        self.Attribute4_A42 = Attribute4_A42
+        self.Attribute4_A43 = Attribute4_A43
+        self.Attribute6_A61 = Attribute6_A61
+        self.Attribute6_A62 = Attribute6_A62
+        self.Attribute6_A63 = Attribute6_A63
+        self.Attribute6_A64 = Attribute6_A64
+        self.Attribute6_A65 = Attribute6_A65
+        self.Attribute7_A71 = Attribute7_A71
+        self.Attribute7_A72 = Attribute7_A72
+        self.Attribute7_A73 = Attribute7_A73
+        self.Attribute7_A74 = Attribute7_A74
+        self.Attribute7_A75 = Attribute7_A75
+        self.Attribute9_A93 = Attribute9_A93
+        self.Attribute12_A121 = Attribute12_A121
+        self.Attribute12_A122 = Attribute12_A122
+        self.Attribute12_A123 = Attribute12_A123
+        self.Attribute12_A124 = Attribute12_A124
+        self.Attribute14_A143 = Attribute14_A143
+        self.Attribute15_A151 = Attribute15_A151
+        self.Attribute15_A152 = Attribute15_A152
+        self.Attribute15_A153 = Attribute15_A153
+        self.Attribute19_A191 = Attribute19_A191
+        self.Attribute19_A192 = Attribute19_A192
